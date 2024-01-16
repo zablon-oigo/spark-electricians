8ef0c42936f3c7abfa68325c81165ad5368a6ca5
zablon <zablonoigo51@gmail.com>
2024-01-16 22:10:06 +0300

    Adding About Component to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 
+import About from "./homepage/About"
 import Hero from "./homepage/Hero"
 
 function Home() {
@@ -7,6 +8,7 @@ function Home() {
   return (
     <>
 <Hero/>
+<About/>
     </>
   )
 }
