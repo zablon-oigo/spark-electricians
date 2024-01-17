bcd2a2fec6dd9fbc52952187f5b62479fa78cb9d
zablon <zablonoigo51@gmail.com>
2024-01-17 12:21:34 +0300

    Styling Services Card

--- src/components/homepage/Services.jsx
+++ src/components/homepage/Services.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import ServicesCard from '../../layout/ServicesCard'
 
 export const Services = () => {
+    useEffect(() => {
+        AOS.init();
+     }, []);
   return (
     <>
-    
+    <div className="min-h-[50vh] bg-gray-200 md:px-20 md:py-20 px-5 py-5">
+        <div className="" data-aos="fade-up" data-easing="ease-in-out" data-aos-duration="3000">
+        <ServicesCard/>
+        </div>
+    </div>
     </>
   )
 }
