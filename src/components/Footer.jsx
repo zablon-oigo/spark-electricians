504c707aa78bfa209e0efd6b8b2d2d38753110db
zablon <zablonoigo51@gmail.com>
2024-01-18 12:33:26 +0300

    Page Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+    <div className="bg-gradient-to-br from-gray-900 to-slate-800">
+        <p className="text-center text-white font-semibold py-3">Copyright © 2023 Spark Electricians. All Rights Reserved.</p>
+    </div>
+    </>
   )
 }
 
