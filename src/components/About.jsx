e83da3312f45b3d6c7155437d77f5f40d3c84579
zablon <zablonoigo51@gmail.com>
2024-01-19 21:37:49 +0300

    Company Profile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,12 +14,20 @@ function About() {
     <>
     <div className="min-h-screen ">
       <div className=" relative -z-10" style={backgroundImage}>
-        <div className="bg-black/70  absolute top-0 right-0 w-full h-[350px] md:px-20 py-32">
+        <div className="bg-black/70  absolute top-0 right-0 w-full h-[350px] md:px-20 px-5 py-32">
           <h2 className="md:text-6xl text-3xl text-white text-center font-semibold">
             About Us
           </h2>
         </div>
       </div>
+      <div className="my-10 md:px-20 px-5">
+          <h2 className="mb-4 text-center md:text-6xl text-2xl font-semibold">
+            Company Profile
+          </h2>
+          <p className="text-xl font-light">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed amet, quibusdam enim quis laboriosam impedit doloribus nemo ab neque illum tempore magni unde? Corrupti, ipsam saepe accusamus facilis rem, corporis blanditiis, repellendus quas vitae at consectetur asperiores optio iusto esse.
+          </p>
+        </div>
     </div>
     </>
   )
