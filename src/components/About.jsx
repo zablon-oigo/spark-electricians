16fb28e3577c216a3d634a15bc64e8e523d5d447
zablon <zablonoigo51@gmail.com>
2024-01-19 21:33:03 +0300

    Page jumbotron

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import about from '../assets/images/about.jpg'
 function About() {
+  const backgroundImage = {
+    backgroundImage: `url(${about})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '350px',  
+    width: '100%'
+
+  }
   return (
-    <div>About</div>
+    <>
+    <div className="min-h-screen ">
+      <div className=" relative -z-10" style={backgroundImage}>
+        <div className="bg-black/70  absolute top-0 right-0 w-full h-[350px] md:px-20 py-32">
+          <h2 className="md:text-6xl text-3xl text-white text-center font-semibold">
+            About Us
+          </h2>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
