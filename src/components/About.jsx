3bda1e12bfa74c61ed5e756624a7a85d8d2d72cd
zablon <zablonoigo51@gmail.com>
2024-01-20 12:40:05 +0300

    Company profile

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import about from '../assets/images/about.jpg'
+import award1 from '../assets/images/award1.png'
+import award2 from '../assets/images/award2.png'
+import award3 from '../assets/images/award3.png'
+
 function About() {
+  const [activeSection, setActiveSection] = useState('Mission');
+
+  const toggleSection = (section) => {
+    setActiveSection(section);
+  };
   const backgroundImage = {
     backgroundImage: `url(${about})`,
     backgroundSize: 'cover',
@@ -10,6 +19,7 @@ function About() {
     width: '100%'
 
   }
+  
   return (
     <>
     <div className="min-h-screen ">
@@ -28,10 +38,63 @@ function About() {
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed amet, quibusdam enim quis laboriosam impedit doloribus nemo ab neque illum tempore magni unde? Corrupti, ipsam saepe accusamus facilis rem, corporis blanditiis, repellendus quas vitae at consectetur asperiores optio iusto esse.
           </p>
         </div>
-        <div className="my-10 h-[300px] bg-gray-100 md:px-20 px-5 ">
-          <div className="flex items-center justify-center gap-2">
-            <div className="w-1/3"></div>
-            <div className="w-1/3"></div>
+        <div className="my-10 h-[600px] bg-gray-100 md:px-20 px-5 ">
+          <div className="flex items-center flex-col md:flex-row justify-center gap-2">
+            <div className="w-1/3 py-6">
+              <h2 className="text-4xl font-semibold mb-10">
+                Accolades
+              </h2>
+              <div className="flex items-center justify-center gap-6">
+                <img src={award1} alt="" />
+                <img src={award2} alt="" className="" />
+                <img src={award3} alt="" />
+              </div>
+            </div>
+            <div className="w-3/4">
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+      <div className=" ">
+        <h2
+          className="text-4xl font-semibold border-b-2 cursor-pointer"
+          onClick={() => toggleSection('Mission')}
+        >
+          Mission
+        </h2>
+        {activeSection === 'Mission' && (
+          <div >
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, quis dolores dignissimos ad quaerat rem id sapiente incidunt accusamus totam obcaecati sunt, enim non nisi eum qui eius laboriosam error harum quibusdam autem soluta illum necessitatibus consequuntur! Saepe, voluptatibus distinctio.
+          </div>
+        )}
+      </div>
+
+      <div className="">
+        <h2
+          className="text-4xl font-semibold cursor-pointer"
+          onClick={() => toggleSection('Vision')}
+        >
+          Vision
+        </h2>
+        {activeSection === 'Vision' && (
+          <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, quis dolores dignissimos ad quaerat rem id sapiente incidunt accusamus totam obcaecati sunt, enim non nisi eum qui eius laboriosam error harum quibusdam autem soluta illum necessitatibus consequuntur! Saepe, voluptatibus distinctio.
+          </div>
+        )}
+      </div>
+
+      <div className="">
+        <h2
+          className="text-4xl font-semibold cursor-pointer"
+          onClick={() => toggleSection('Values')}
+        >
+          Values
+        </h2>
+        {activeSection === 'Values' && (
+          <div>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla, quis dolores dignissimos ad quaerat rem id sapiente incidunt accusamus totam obcaecati sunt, enim non nisi eum qui eius laboriosam error harum quibusdam autem soluta illum necessitatibus consequuntur! Saepe, voluptatibus distinctio.
+          </div>
+        )}
+      </div>
+    </div>
+            </div>
           </div>
 
         </div>
