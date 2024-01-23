19d5ed03c7da853298d01c33a14659cb2f824bd9
zablon <zablonoigo51@gmail.com>
2024-01-23 17:46:30 +0300

    BgImage

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Button from '../layout/Button'
 import { NavLink } from "react-router-dom"
+import notFound from '../assets/images/404.png'
 function NotFound() {
+  const bgImages={
+    backgroundImage: `url(${notFound})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+
+  }
   return (
     <>
-    <div className="min-h-screen md:px-20 md:py-20 px-5 py-5">
+    <div className="min-h-screen md:px-20 md:py-20 px-5 py-5" style={bgImages}>
  <div className="my-40 flex items-center gap-6 justify-center flex-col">
  <h1>Page Not Found </h1>
       <p>
