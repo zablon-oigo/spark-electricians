96ae2579652a2db8c135762b5d29a81569080ed1
zablon <zablonoigo51@gmail.com>
2024-01-25 11:31:32 +0300

    Padding on Y-axis

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Faq() {
   return (
     <>
-    <div className="min-h-screen bg-gradient-to-r from-slate-500 to-gray-800"></div>
+    <div className="min-h-screen bg-gradient-to-r from-slate-500 to-gray-800 md:px-20 md:py-20 px-5 py-10">
+      <div className=""></div>
+    </div>
     </>
   )
 }
