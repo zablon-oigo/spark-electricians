76edba3dbfeb6b53fe97d339ec51eb4cf2433a67
zablon <zablonoigo51@gmail.com>
2024-01-25 11:34:16 +0300

    Heading

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,7 +4,11 @@ function Faq() {
   return (
     <>
     <div className="min-h-screen bg-gradient-to-r from-slate-500 to-gray-800 md:px-20 md:py-20 px-5 py-10">
-      <div className=""></div>
+      <div className="">
+        <h2 className="capitalize text-white text-6xl text-center font-semibold">
+          frequently asked questions
+        </h2>
+      </div>
     </div>
     </>
   )
