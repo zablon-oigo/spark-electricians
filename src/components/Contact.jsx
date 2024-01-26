20825f66cc816b331bc362dacd034b25d2d37579
zablon <zablonoigo51@gmail.com>
2024-01-26 09:58:26 +0300

    Style: Styling Contact Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Contact() {
   return (
     <>
-    <div className="min-h-screen bg-gradient-to-tr from-slate-50 to-gray-100 md:px-20 md:py-20 px-5 py-10">
-      <h2 className="md:text-6xl text-3xl font-semibold text-gray-600 text-center">
+    <div className="min-h-screen bg-gradient-to-b from-slate-900 to-gray-800  text-white md:px-20 md:py-20 px-5 py-10">
+      <h2 className="md:text-6xl  text-3xl font-semibold  text-center">
         Contact
       </h2>
       <div className="my-40">
