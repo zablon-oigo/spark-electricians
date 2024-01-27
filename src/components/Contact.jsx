c595d18397bce1dfe51f22e1d711060265d81c31
zablon <zablonoigo51@gmail.com>
2024-01-27 20:12:56 +0300

    Style: Grid Layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,35 +1,31 @@
 import React from 'react'
-
+import ContactForm from '../layout/ContactForm'
 function Contact() {
   return (
     <>
     <div className="min-h-screen bg-gradient-to-b from-slate-900 to-gray-800  text-white md:px-20 md:py-20 px-5 py-10">
+      <p className="text-md md:text-lg text-gray-400 capitalize text-center">don't be a stranger</p>
       <h2 className="md:text-6xl  text-3xl font-semibold  text-center">
-        Contact
+        Contact Us
       </h2>
       <div className="my-40">
-        <div className="flex items-center justify-between flex-col md:flex-row ">
-          <div className="">
-            <h2 className="">
-              head office
-            </h2>
-            <p className="">
-
-            </p>
-          </div>
-          <div className="">
-            <img src="" alt="" className="" />
-          </div>
-        </div>
-        <div className="flex items-center justify-between flex-col md:flex-row">
-          <div className="">
-            <h2 className="">
-              regional offices
-            </h2>
-          </div>
+        
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <div className="">
             
           </div>
+         
+        </div>
+        <div className="">
+          <p className='text-md md:text-lg text-gray-400 capitalize text-center'>
+            message us
+          </p>
+        <h2 className="md:text-6xl  text-3xl font-semibold  text-center capitalize">
+          get in touch
+        </h2>
+        <div className="my-10">
+          <ContactForm/>
+        </div>
         </div>
       </div>
     </div>
