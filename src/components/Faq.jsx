df5da1d33e06ae8be51847eff47d8ed9aa693da7
zablon <zablonoigo51@gmail.com>
2024-01-29 12:43:31 +0300

    Style : Bg Color

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 function Faq() {
   return (
     <>
-    <div className="min-h-screen bg-gradient-to-r from-slate-500 to-gray-800 md:px-20 md:py-20 px-5 py-10">
+    <div className="min-h-screen bg-gradient-to-b from-slate-900 to-gray-800  text-white md:px-20 md:py-20 px-5 py-10">
       <div className="">
         <h2 className="capitalize text-white text-6xl text-center font-semibold">
           frequently asked questions
         </h2>
+        <div className="my-20 bg-white shadow-2xl">
+
+        </div>
       </div>
     </div>
     </>
