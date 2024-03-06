65a1d4694655d66269c349b1c4c9d34f58579fde
zablon <zablonoigo51@gmail.com>
2024-03-06 21:55:38 +0300

    styling about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 import React,{useState} from 'react'
 import about from '../assets/images/about.jpg'
-// import award1 from '../assets/images/award1.png'
-// import award2 from '../assets/images/award2.png'
-// import award3 from '../assets/images/award3.png'
+import award1 from '../assets/images/award1.png'
+import award2 from '../assets/images/award2.png'
+import award3 from '../assets/images/award3.png'
 
 function About() {
   const backgroundImage = {
@@ -17,7 +17,7 @@ function About() {
   
   return (
     <>
-    <div className="min-h-screen ">
+    <div className="min-h-screen  ">
       <div className=" relative -z-10" style={backgroundImage}>
         <div className="bg-black/70  absolute top-0 right-0 w-full h-[350px] md:px-20 px-5 py-32">
           <h2 className="md:text-6xl text-3xl text-white text-center font-semibold">
@@ -25,7 +25,13 @@ function About() {
           </h2>
         </div>
       </div>
-      <div className=""></div>
+      <div className="h-screen bg-gradient-to-b from-slate-900 to-gray-800 py-20">
+                     <div className="grid grid-cols-3 place-content-center justify-items-center">
+                     <img src={award1} alt="" className="bg-white rounded-lg" />
+                      <img src={award2} alt="" className="bg-white rounded-lg" />
+                      <img src={award3} alt="" className="bg-white rounded-lg" />
+                     </div>
+      </div>
     </div>
     </>
   )
